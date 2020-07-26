c26598388b773d138d401328d695df1484c49c3f
OlgaShkarat <olgashkarat@outlook.com>
2020-07-26 13:20:05 +0300

    update

--- ChangeColor/View/MyView.swift
+++ ChangeColor/View/MyView.swift
@@ -16,44 +16,41 @@ struct MyView: View {
     
     var body: some View {
         
-        let formatter = NumberFormatter()
-        formatter.numberStyle = NumberFormatter.Style.decimal
-        
-        return
-            HStack {
-                Text(nameColor)
-                    .foregroundColor(.white)
-                    .frame(width: 50, alignment: .center)
+        HStack {
+            Text(nameColor)
+                .foregroundColor(.white)
+                .frame(width: 50, alignment: .center)
+            
+            Text("\(lround(sliderValue))")
+                .foregroundColor(.white)
+                .frame(width: 50, alignment: .center)
+            
+            Slider(value: Binding(
+                get: {
+                    self.sliderValue
+            },
+                set: { (newValue) in
+                    self.sliderValue = newValue
+            }), in: 0...255, step: 1)
+                .accentColor(accentColor).frame(alignment: .center)
+            
+            TextField("", value: $sliderValue, formatter: NumberFormatter()) {
                 
-                Text("\(lround(sliderValue))")
-                    .foregroundColor(.white)
-                    .frame(width: 50, alignment: .center)
-                
-                
-                Slider(value: Binding(
-                    get: {
-                        self.sliderValue
-                },
-                    set: { (newValue) in
-                        self.sliderValue = newValue
-                }), in: 0...255, step: 1)
-                    .accentColor(accentColor).frame(alignment: .center)
-        
-                TextField("", value: $sliderValue, formatter: formatter) {
-                    let value = self.sliderValue
-                    switch value {
-                    case ..<0:
-                        self.sliderValue = 0
-                    case 0...255:
-                        self.sliderValue = value
-                    default:
-                        self.sliderValue = 255
-                    }
+                let value = self.sliderValue
+                switch value {
+                case ..<0:
+                    self.sliderValue = 0
+                case 0...255:
+                    self.sliderValue = value
+                default:
+                    self.sliderValue = 255
                 }
+            }
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .frame(width: 50, alignment: .center)
-            }
-            .frame(width: UIScreen.main.bounds.width - 20)
-            .padding()
+            
+        }
+        .frame(width: UIScreen.main.bounds.width - 20)
+        .padding()
     }
 }
